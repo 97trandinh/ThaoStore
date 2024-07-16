fa91d0d363e3871b27ae993a346d84a6ff7e762a
97trandinh <97trandinh1@gmail.com>
2024-07-16 03:00:47 -0500

    thao

---
+++ js/api.js
@@ -0,0 +1,11 @@
+const urlCategory ="http://localhost:3000/categories";
+//#region Get All
+async function getAll(url, callback) {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        callback(data);
+    } catch (error) {
+        console.error( "lỗi:", error);
+    }
+}
--- js/home-admin.js
+++ js/home-admin.js
@@ -1,3 +1,4 @@
+//#region xử lý hiện ẩn của side menu
 let span= document.querySelectorAll(".left span");
 let icon= document.querySelector(".left .icon ");
 let i= document.querySelectorAll(".left .icon i");
@@ -7,3 +8,14 @@ icon.addEventListener("click", () => {
     i.forEach(element=> element.classList.toggle("hidden"));
     menu.classList.toggle("nav-menu");
 })
+//#endregion
+//#region xử lý click menu
+ let listItem= document.querySelectorAll (".nav .nav-item a");
+ let listBox = document.querySelectorAll (".right-main .box");
+ listItem.forEach((element, index) => element.addEventListener("click", ()=> {
+    listBox.forEach( listBox => listBox.style.display =" none");
+    listBox[index].style.display = "block";
+    listItem.forEach(item => item.style.color="white");
+    element.style.color = "red";
+ }));
+//#endregion
